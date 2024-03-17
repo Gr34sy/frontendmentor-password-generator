cf8bcf94ab86c4e4981d41e80ab1f8779facfd39
Gr34sy <slupek3@poczta.fm>
2024-03-17 14:14:15 +0100

    initialized react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Password Generator P4$5W0rD! Character Length Include Uppercase Letters
+      Include Lowercase Letters Include Numbers Include Symbols Strength
+      Generate
+    </div>
+  );
+}
+
+export default App;
